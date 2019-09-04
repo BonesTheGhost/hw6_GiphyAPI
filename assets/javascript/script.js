77fc9e41a8e2a06c0079e127062fd151aaa09f9f
BonesTheGhost <22923497+BonesTheGhost@users.noreply.github.com>
2019-09-04 13:59:08 -0500

    [1] INITIAL COMMIT :: Made file tree and added the necessary base files.

---
+++ assets/javascript/script.js
@@ -0,0 +1,7 @@
+
+//The API base address. All queries begin with a "?" ???
+var api = "https://api.giphy.com/v1/gifs/search?";
+//My custome key for the API provided by the developer account that I signed up for.
+var apiKey = "&api_key=dVEhj3UXigqQgcr4tnFlTClyMXCEgZhZ";
+//My SEARCH query.
+var query = "&q=rainbow"
